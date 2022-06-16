7eff5ae36517a56cbd61262a9cb59e2763443a7b
ianfleming464 <ianflemingdeveloper@gmail.com>
2022-06-16 22:03:22 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import { useState } from 'react';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: userName, age: userAge, id: Math.random().toString() }];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,44 @@
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
+import { useState } from 'react';
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' value={enteredUsername} onChange={usernameChangeHandler} />
+        <label htmlFor='age'>Age (years)</label>
+        <input type='age' id='username' value={enteredAge} onChange={ageChangeHandler} />
+        <Button type='submit'>Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
